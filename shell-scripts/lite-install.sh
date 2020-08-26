796e9baa6a84d7a6bd7f8c48e76703a137da7016
Jacob MacLeod <48350114+jacob-macleod@users.noreply.github.com>
2020-08-26 10:16:40 +0100

    Update lite-install.sh

--- shell-scripts/lite-install.sh
+++ shell-scripts/lite-install.sh
@@ -2,6 +2,8 @@
 #For debian
 sudo apt-get update
 sudo apt-get install git
+
+#Vim install
 sudo apt-get install vim
 mkdir .vim
 mkdir .vim/colors
@@ -9,11 +11,16 @@ wget https://raw.githubusercontent.com/jacob-macleod/jacob-macleod/master/dot-fi
 mv vimrc .vimrc
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 sudo apt-get install vim-gtk3 --fix-missing
+
 sudo apt-get install gnome-system-monitor
+
+#VSCode install
 wget https://go.microsoft.com/fwlink/?LinkID=760868
 mv index.html?LinkID=760868 vscode.deb
 sudo apt-get install libxkbfile1
 sudo dpkg --install vscode.deb
+
+#ZSH install
 sudo apt-get install zsh
 sudo chsh -s /bin/zsh jacob
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
@@ -21,6 +28,12 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 wget https://raw.githubusercontent.com/jacob-macleod/jacob-macleod/master/dot-files/.zshrc
 mv .zshrc.1 .zshrc
 
+#JVM install
+
+#Eclipse install
+wget http://ftp.fau.de/eclipse/technology/epp/downloads/release/2020-06/R/eclipse-java-2020-06-R-linux-gtk-x86_64.tar.gz
+tar -xzf eclipse-java-2020-06-R-linux-gtk-x86_64.tar.gz
+./eclipse/eclipse
 
 echo "Finally, enter   git config --global user.email 'your email'"
 echo "and git config --global user.name 'Your Name'"
