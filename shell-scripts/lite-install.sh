3d4584f1796785dad822e2d080578533e713df38
Jacob MacLeod <48350114+jacob-macleod@users.noreply.github.com>
2020-09-10 14:19:38 +0100

    Update lite-install.sh

--- shell-scripts/lite-install.sh
+++ shell-scripts/lite-install.sh
@@ -45,5 +45,49 @@ sudo apt-get install xserver-xephyr
 sudo apt-get install i3
 wget https://raw.githubusercontent.com/jacob-macleod/jacob-macleod/master/shell-scripts/wm.sh
 
+#I3 configuration: {
+#Install Yosenemite font
+wget https://github.com/supermarin/YosemiteSanFranciscoFont/archive/master.zip
+unzip master.zip
+mkdir .fonts
+mv YosemiteSanFranciscoFont-master/*.ttf ~/.fonts/
+sudo apt --fix-broken-install
+
+sudo apt-get install lxappearance
+sudo apt-get install thunar
+sudo apt-get install rofi
+
+#Install arc GTK theme
+sudo apt-get install arc-theme
+
+#Install the moka icon theme
+wget https://snwh.org/moka/download.php?owner=snwh&ppa=ppa&pkg=moka-icon-theme,18.04
+mv 'download.php?owner=snwh&ppa=ppa&pkg=moka-icon-theme,18.04' moka.deb
+sudo dpkg --install moka.deb
+sudo apt --fix-broken install
+
+#Configure the bottom bar
+sudo apt-get install i3blocks
+wget https://github.com/jacob-macleod/jacob-macleod/raw/master/dot-files/i3blocks.conf
+mv i3blocks.conf $HOME/.config/i3
+rm etc/i3blocks.conf
+
+#Set background
+wget https://i.pinimg.com/originals/3f/a0/40/3fa040b186dbfc221b96b62c7083c03b.jpg
+sudo apt-get install feh
+feh --bg-scale 3fa040b186dbfc221b96b62c7083c03b.jpg 
+
+#Installing termite
+wget https://github.com/jacob-macleod/jacob-macleod/raw/master/shell-scripts/install-termite.sh
+bash install-termite.sh
+
+#Download i3 config
+wget https://github.com/jacob-macleod/jacob-macleod/raw/master/dot-files/config
+mv config $HOME/.config/i3/config
+
+#}
+
+echo "You will need to manually change window features using lxappearance - suggested changes: Change font to SFNS Display. Change the theme in LXappearance to Arc darker. THange the icon theme to moka."
+echo "You will need to manually install arc darker firefox theme"
 echo "Finally, enter   git config --global user.email 'your email'"
 echo "and git config --global user.name 'Your Name'"
